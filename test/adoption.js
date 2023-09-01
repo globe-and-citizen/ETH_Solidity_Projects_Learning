23811087c5b2a802cedc3d361c435d55108168c3
huzaifamk <malikhuzaifa99@gmail.com>
2023-09-01 11:47:13 +0500

    Added a unit test for the Adoption contract

---
+++ test/adoption.js
@@ -0,0 +1,31 @@
+var Adoption = artifacts.require("./Adoption.sol");
+
+contract("Adoption", function (accounts) {
+  it("should assert true", function (done) {
+    var adoption = Adoption.deployed();
+    assert.isTrue(true);
+    done();
+  });
+
+  describe("First group of tests", () => {
+    let instance;
+    before(async () => {
+      instance = await Adoption.deployed();
+    });
+
+    it("User should adopt a pet", async () => {
+      await instance.adopt.sendTransaction(8, { from: accounts[0] });
+      let adopter = await instance.adopters.call(8);
+      assert.equal(adopter, accounts[0], "Incorrect owner address");
+    });
+
+    it("Should get adopter address by pet id in array", async () => {
+      let adopters = await instance.getAdopters.call();
+      assert.equal(
+        adopters[8],
+        accounts[0],
+        "Owner of pet id should be recorded in the array"
+      );
+    });
+  });
+});
