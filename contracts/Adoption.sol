e4fc7f26490b5873d747ba2f079a8f0f23333e81
huzaifamk <malikhuzaifa99@gmail.com>
2023-09-01 11:42:35 +0500

    Made base migrations and test deployed using Ganache

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,8 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Adoption {
-  constructor() public {
-  }
+    address[16] public adopters;
+
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
 }
